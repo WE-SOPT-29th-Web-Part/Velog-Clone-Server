89e06bb3a5f6bb63c50052457e52723cbd3b1a06
Tekiter <tekiter8@gmail.com>
2021-11-27 15:20:27 +0900

    feat: 기본 포트 변경

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { createApp } from "./app";
 
 async function main() {
   const app = await createApp({
-    baseURL: "http://localhost:5000",
-    port: 5000,
+    baseURL: "http://localhost:5005",
+    port: 5005,
   });
   app.start();
 }
